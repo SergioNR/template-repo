6dc886000612ee7d313b8a5f7394c06c3db8e0fc
Sergio Navarro <s.navarroredondo@gmail.com>
2023-12-10 21:51:33 +0100

    ADD npm
    add npm

---
+++ js/exampleModule.js
@@ -0,0 +1,10 @@
+const exampleFunction = () => {
+    console.log('Hello world!');
+    };
+
+export default exampleFunction;
+
+
+const functionOne = () => console.log('FUNCTION ONE!');
+
+export { functionOne };
